6b06f0119413f47096b8f33bc98e89d976dd65c5
biswasoren <messi.biswa@gmail.com>
2021-05-06 10:46:51 +0530

    Beautify Untracked

---
+++ src/components/header/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./header.css";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  static propTypes = {};
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="header">
+        <div className="logo">
+          <img src="icon.svg" alt="icon"></img>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
